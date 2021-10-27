ae8a95dd4e0e74c43500e9250f441c6ef971b18e
Debabrata Saha <sahadebabrata570@gmail.com>
2021-10-27 23:51:37 +0600

    set logged in user in state and displayed logged in user info

--- src/App.js
+++ src/App.js
@@ -8,19 +8,25 @@ initializeAuthentication()
 const provider = new GoogleAuthProvider();
 
 function App() {
-  const [user, setUser] = useState([])
+  const [user, setUser] = useState({})
   const handleGoogleSignIn = () => {
     const auth = getAuth();
     signInWithPopup(auth, provider)
       .then((result) => {
         const credential = GoogleAuthProvider.credentialFromResult(result);
         const token = credential.accessToken;
-        const user = result.user;
-        setUser(user)
-        console.log(user)
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(loggedInUser)
+        console.log(result.user)
       }).catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
+        console.log(errorMessage)
         const email = error.email;
         const credential = GoogleAuthProvider.credentialFromError(error);
       });
@@ -28,9 +34,14 @@ function App() {
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google SignIn</button>
-      <h3>Name: {user.displayName}</h3>
-      {/* <p>Photo url : {user.photoURL}</p> */}
-      <img src={user.photoURL} alt="" />
+      <br />
+      {
+        user.email && <div>
+          <h2>Welcome {user.name}</h2>
+          <p>I know your email address:  {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
     </div>
   );
 }
