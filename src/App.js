76884c19e12beb1ed02a9dad8eea7f370bb20502
Debabrata Saha <sahadebabrata570@gmail.com>
2021-10-27 21:26:14 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import initializeAuthentication from './Firebase/firebase.initialize';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from 'react';
+
+initializeAuthentication()
+const provider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState([])
+  const handleGoogleSignIn = () => {
+    const auth = getAuth();
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        const user = result.user;
+        setUser(user)
+        console.log(user)
+      }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleGoogleSignIn}>Google SignIn</button>
+      <h3>Name: {user.displayName}</h3>
+      {/* <p>Photo url : {user.photoURL}</p> */}
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDNZYC4R5-tmD7LKZp1_x_h30o--S6WAcQ",
+    authDomain: "simple-firebase-authenti-9a7bf.firebaseapp.com",
+    projectId: "simple-firebase-authenti-9a7bf",
+    storageBucket: "simple-firebase-authenti-9a7bf.appspot.com",
+    messagingSenderId: "783978970151",
+    appId: "1:783978970151:web:13519fce227c15d92ba202"
+};
+
+export default firebaseConfig;
